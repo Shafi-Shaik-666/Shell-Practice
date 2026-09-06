bee59ae599552b7cdbb4fa8ccce60b919dcdc74a
shafi-shaik-666 <shafibjr666@gmail.com>
2026-09-06 13:32:58 +0530

    Package installation

---
+++ 02-package-installation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+# check root access or not
+
+if [$USERID -ne 0]; then
+    echo "Run this script with root priviliges"
+    exit 1
+fi
+
+# first arg --> What you are trying to install..
+# second arg --> exit code
+
+dnf list installed mysqld
+if [$? -eq 0]; then
+    echo "MySQL is already installe.. Skipping.."
+else 
+    echo "Installing MySQL"
+    dnf install mysql -y
+fi
