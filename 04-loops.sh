013de2e4de17421fb2076cbcb8bc3a2a51a1a60b
shafi-shaik-666 <shafibjr666@gmail.com>
2026-09-07 06:39:59 +0530

    Package installation

--- 04-loops.sh
+++ 04-loops.sh
@@ -23,7 +23,14 @@ VALIDATE(){
     fi
 }
 
-for package in $@
+for package in "$@"
 do 
     echo "Installing $package"
+    dnf list installed $package
+    if [ $? -ne 0 ];then
+    dnf install $package -y &>> $LOGS_FILE 
+    VALIDATE "Installing $package" $?
+    else
+        echo "$package is already installed.. SKIPPING"
+    fi
 done
