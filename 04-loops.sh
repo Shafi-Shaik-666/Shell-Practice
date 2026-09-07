5ccc6b7c7ee55988d22b36abed07b00b14ec02cf
shafi-shaik-666 <shafibjr666@gmail.com>
2026-09-07 06:27:04 +0530

    Package installation

---
+++ 04-loops.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_DIR=/var/log/shell-script
+LOGS_FILE="$LOGS_DIR/$0.log"
+
+# check root access or not
+
+if [ $USERID -ne 0 ]; then
+    echo "Run this script with root priviliges"
+    exit 1
+fi
+
+# first arg --> What you are trying to install..
+# second arg --> exit code
+
+VALIDATE(){
+    if [ $2 -ne 0 ];then
+        echo "Installing $1 is failed.."
+      else
+        echo "Installing $1 is success.."
+    fi
+}
+
+for package in "$@"
+do 
+    echo "Installing $package"
+done
